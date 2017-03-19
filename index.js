d071976e384ebfdb66515c1784559f438a54ccd1
Thomas McCormick <thomasmccormick@curalate.com>
2017-03-18 17:29:10 -0700

    Stylish: Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const aws = require('aws-sdk');
+const phantomjs = require('phantomjs-prebuilt');
+
+exports.handler = function(event, context, callback) {
+  const phantom = phantomjs.exec('phjs-main.js');
+
+  phantom.stdout.on('data', function(buf) {
+    console.log('[STR] stdout "%s"', String(buf));
+  });
+
+  phantom.stderr.on('data', function(buf) {
+    console.log('[STR] stderr "%s"', String(buf));
+  });
+
+  phantom.on('close', function(code) {
+    console.log('[END] code', code);
+  });
+
+  phantom.on('exit', code => {
+    callback(null, 'Execution complete!');
+  });
+};
---
+++ phjs-main.js
@@ -0,0 +1,5 @@
+console.log('Starting Phantom!');
+setTimeout(function() {
+  console.log('Hello world!');
+  phantom.exit();
+}, 1000);
