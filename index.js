c7370510f3a5b6217120b2d22431b86c47491205
Thomas McCormick <thomasmccormick@curalate.com>
2017-03-19 18:26:49 -0700

    Add png2palette

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const aws = require('aws-sdk');
-const phantomjs = require('phantomjs-prebuilt');
+const Aws = require('aws-sdk');
+const Canvas = require('canvas');
+const Image = Canvas.Image;
+const PhantomJs = require('phantomjs-prebuilt');
+const RgbQuant = require('rgbquant');
 
 function parseJson(str) {
   try {
@@ -13,19 +16,45 @@ function parseJson(str) {
   return undefined;
 }
 
-function phantomHandler(callback, msgRaw) {
-  const msg = parseJson(msgRaw);
-  if (!msg || !msg.status || msg.status !== 'ok') {
-    console.log('Phantom: "%s"', String(msgRaw));
+function parseMsg(raw) {
+  const msg = parseJson(raw);
+
+  if (msg && msg.status && msg.status === 'ok') {
+    return msg;
+  }
+
+  return undefined;
+}
+
+function png2palette(complete, data) {
+  const img = new Image;
+	img.src = data;
+
+	const can = new Canvas(img.width, img.height);
+	const ctx = can.getContext('2d');
+	ctx.drawImage(img, 0, 0, img.width, img.height);
+
+	const q = new RgbQuant({ colors: 8 });
+	q.sample(can);
+
+  const palette = q.palette(true);
+
+	complete(null, palette);
+}
+
+function phantomHandler(complete, raw) {
+  const msg = parseMsg(raw);
+  if (!msg) {
+    console.log('Phantom: "%s"', String(raw));
     return;
   }
 
   const screenshot = new Buffer(msg.body, 'base64');
-  callback(null, 'Execution was successful!');
+  png2palette(complete, screenshot);
 }
 
 exports.handler = function(event, context, callback) {
-  const phantom = phantomjs.exec('phjs-main.js', 'http://google.com/');
+  const phantom = PhantomJs.exec('phjs-main.js', 'http://google.com/');
 
   phantom.stdout.on('data', (msg) => phantomHandler(callback, String(msg)));
   phantom.stderr.on('data', (err) => callback(String(err)));
