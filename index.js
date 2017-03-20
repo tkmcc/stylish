a9eabf85aaecf58b81d3f41e7b927a94b1e52eee
Thomas McCormick <thomasmccormick@curalate.com>
2017-03-19 21:04:23 -0700

    Rip out node-canvas and rewrite png2palette

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const Canvas = require('canvas');
 const PhantomJs = require('phantomjs-prebuilt');
+const Png = require('pngjs').PNG;
 const RgbQuant = require('rgbquant');
 
-const Image = Canvas.Image;
+const PHANTOM_ARGS = {
+  url: 'http://google.com/',
+  viewSize: {
+    width: 1920,
+    height: 1080,
+  },
+};
 
 function parseJson(str) {
   try {
@@ -27,16 +33,29 @@ function parseMsg(raw) {
   return undefined;
 }
 
-function png2palette(complete, data) {
-  const img = new Image();
-  img.src = data;
+function png2palette(complete, png) {
+  const rgb = png.data;
 
-  const can = new Canvas(img.width, img.height);
-  const ctx = can.getContext('2d');
-  ctx.drawImage(img, 0, 0, img.width, img.height);
+  // Turn array of {R, G, B, A} into array of 32 bit pixels
+  const pixels = rgb.reduce((acc, val, index) => {
+    const pos = (index % 4);
+    if (pos === 0) {
+      acc.push(0);
+    }
+
+    const pixel = acc.pop();
+    const color = val & 0xff;
+    const shiftBits = 8 * pos;
+
+    acc.push(pixel & (color << shiftBits));
+
+    return acc;
+  }, []);
+
+  const u32pixels = new Uint32Array(pixels);
 
   const q = new RgbQuant({ colors: 8 });
-  q.sample(can);
+  q.sample(u32pixels);
 
   const palette = q.palette(true);
 
@@ -51,11 +70,13 @@ function phantomHandler(complete, raw) {
   }
 
   const screenshot = new Buffer(msg.body, 'base64');
-  png2palette(complete, screenshot);
+
+  const png = new Png({ filterType: 4 });
+  png.parse(screenshot, png2palette.bind(complete, png));
 }
 
 exports.handler = function (event, context, callback) {
-  const phantom = PhantomJs.exec('phjs-main.js', 'http://google.com/');
+  const phantom = PhantomJs.exec('phjs-main.js', JSON.stringify(PHANTOM_ARGS));
 
   phantom.stdout.on('data', msg => phantomHandler(callback, String(msg)));
   phantom.stderr.on('data', err => callback(String(err)));
--- phjs-main.js
+++ phjs-main.js
@@ -2,12 +2,14 @@
 
 var system = require('system');
 var webpage = require('webpage');
+
 var page = webpage.create();
-var url = system.args[1];
 var ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) ' +
          'AppleWebKit/537.36 (KHTML, like Gecko) ' +
          'Chrome/56.0.2924.87 Safari/537.36';
-var viewSize = { width: 1920, height: 1080 };
+var args = JSON.parse(system.args[1]);
+var url = args.url;
+var viewSize = args.viewSize;
 
 
 function complete(msg) {
